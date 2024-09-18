92cc33a1326d0b910e769a1aa37100b3b2a974a2
kaaduarte <programacaoduarte@gmail.com>
2024-09-18 11:02:29 -0300

    atualização

--- script.js
+++ script.js
@@ -34,10 +34,15 @@ function updateCirclePosition() {
 }
 
 // Função criar quadrados //
-function createSquares()
-{
+function createSquares() {
     clearShapes();
     gameArea.style.backgroundColor = rgb(102, 129, 102);
 
-    for
+    for (let i = 0; i < 4; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.left = `${Math.random() * (window.innerWidth - 50)}px`;
+        square.style.top = `${Math.random() * (window.innerHeight - 50)}px`;
+        gameArea.appendChild(square);
+    }
 }
