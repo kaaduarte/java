8c49428f97941371a3c06dbe20485fbe2c39293e
kaaduarte <programacaoduarte@gmail.com>
2024-09-18 09:49:18 -0300

    JS

---
+++ script.js
@@ -0,0 +1,9 @@
+// Pegando o círculo e a área de jogo //
+const circle = document.getElementById('circle');
+const gameArea = document.getElementById('gameArea');
+
+let posX = 50;
+let posY = 50;
+
+const moveSpeed = 10;
+const originalBackgroundColor = 'rgb(103, 147, 197)'; // Cor original de fundo //
