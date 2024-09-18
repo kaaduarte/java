bd0394d9eabdf2c9854a35480edf86ae45776d6f
kaaduarte <programacaoduarte@gmail.com>
2024-09-18 10:30:41 -0300

    att

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ let posY = 50;
 const moveSpeed = 10;
 const originalBackgroundColor = 'rgb(103, 147, 197)'; // Cor original de fundo //
 
+// Função que move o círculo //
+document.addEventListener('keydown', (event) => {
+    switch (event.code) {
+        case 'ArrowUp':
+            if (posY > 0) posY -= moveSpeed;
+            break;
+        case 'ArrowDown':
+            if (posY < window.innerHeight - circle.offsetHeight) posY += moveSpeed;
+            break;
+        case 'ArrowLeft':
+            if (posX > 0) posX -= moveSpeed;
+            break;
+        case 'ArrowRight':
+            if (posX < window.innerWidth - circle.offsetHeight) posX += moveSpeed;
+            break;
+    }
+updateCirclePosition();
+});
+
+// Função que atualiza a posição do círculo //
+function updateCirclePosition () {
+    circle.style.left = `${posX}px`;
+    circle.style.top = `${posY}px`;
+}
+
